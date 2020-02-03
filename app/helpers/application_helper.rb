5794358a7f06a8c56efcf380dc5bfad37d29b46c
Josh Pigford <josh@joshpigford.com>
2020-02-02 18:48:55 -0600

    Fix for missing date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,10 +21,11 @@ module ApplicationHelper
   end
 
   def last_updated
-    Rails.cache.fetch('last_updated', expires_in: 12.hours) do
+    last_updated = Rails.cache.fetch('last_updated', expires_in: 12.hours) do
       response = HTTParty.get('https://api.github.com/repos/Shpigford/joshpigford.com')
       json = JSON.parse(response.body)
       json['pushed_at']
     end
+    last_updated.present? ? last_updated : Time.now
   end
 end
