9acce83598155d121478dc94798d8706a9eb2256
Josh Pigford <josh@joshpigford.com>
2020-02-02 19:41:52 -0600

    Toy title tweak

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,11 +1,12 @@
 class ToysController < ApplicationController
   def index
     @toys = Toy.all
-    @title = 'Toys'
+    @title = 'Designer Toys'
     @description = "My designer toy collection."
   end
 
   def show
     @toy = Toy.find(params[:id])
+    @title = "#{@toy.name} - Designer Toys"
   end
 end
