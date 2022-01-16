1febac4909cfd2264810197dd59b16a48ce3cafc
Josh Pigford <josh@joshpigford.com>
2022-01-16 13:26:56 -0600

    Switch IPFS provider

--- app/helpers/nfts_helper.rb
+++ app/helpers/nfts_helper.rb
@@ -2,7 +2,8 @@ module NftsHelper
   def nft_image(url)
     if url.include?('ipfs:')
       token = url.gsub('ipfs://','')
-      "https://cloudflare-ipfs.com/ipfs/#{token}/"
+      #"https://cloudflare-ipfs.com/ipfs/#{token}/"
+      "https://fxparty.mypinata.cloud/ipfs/#{token}"
     else
       url
     end
